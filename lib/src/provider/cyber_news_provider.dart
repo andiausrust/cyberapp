4ea51a42ac6cec8948ca7a92f6a0e7bcbd674351
Andi <familie.mayer@aon.at>
2019-03-17 18:34:14 +0100

    adjusted model properties - CyberNews - for null values -> n/a

--- lib/src/provider/cyber_news_provider.dart
+++ lib/src/provider/cyber_news_provider.dart
@@ -13,13 +13,20 @@ class CyberNewsProvider implements INewsSource {
         '${ProviderConstants.root}?q=${ProviderConstants.queryNews}&sortBy=${ProviderConstants.sortBy}&language=${ProviderConstants.language}&pageSize=${ProviderConstants.pageSize}&apiKey=${ProviderConstants.apiKey}');
     final news = json.decode(response.body);
     var data = news["articles"];
-    print('###########');
-    print(data.length);
-    print(data.runtimeType);
 
     List<CyberNews> cyberNews = List<CyberNews>();
 
     for (int i = 0; i < data.length; i++) {
+      if (data[i]["source"]["name"] == null) data[i]["source"]["name"] = "n/a";
+      if (data[i]["author"] == null) data[i]["author"] = "n/a";
+      if (data[i]["title"] == null) data[i]["title"] = "n/a";
+      if (data[i]["description"] == null) data[i]["description"] = "n/a";
+      if (data[i]["url"] == null) data[i]["url"] = "n/a";
+      if (data[i]["urlToImage"] == null)
+        data[i]["urlToImage"] = "https://loremflickr.com/320/240";
+      if (data[i]["publishedAt"] == null) data[i]["publishedAt"] = "n/a";
+      if (data[i]["content"] == null) data[i]["content"] = "n/a";
+
       var cyberNew = CyberNews.fromJson(
           data[i]["source"]["name"],
           data[i]["author"],
@@ -30,8 +37,6 @@ class CyberNewsProvider implements INewsSource {
           data[i]["publishedAt"],
           data[i]["content"]);
       cyberNews.add(cyberNew);
-      print(cyberNews.length);
-      print(cyberNews[i].author);
     }
 
     return cyberNews;
--- lib/src/screens/cyber_news_list.dart
+++ lib/src/screens/cyber_news_list.dart
@@ -19,16 +19,38 @@ class CyberNewsList extends StatelessWidget {
       stream: bloc.cyberNews,
       builder: (context, AsyncSnapshot<List<CyberNews>> snapshot) {
         if (!snapshot.hasData) {
-          return Text('still waiting');
+          return Center(
+            child: CircularProgressIndicator(),
+          );
         }
-
         return ListView.builder(
           itemCount: snapshot.data.length,
           itemBuilder: (context, int index) {
-            return Text(snapshot.data[index].author);
+            return cyberContent(context, index, snapshot);
           },
         );
       },
+    );
+  }
+
+  Widget cyberContent(BuildContext context, int index,
+      AsyncSnapshot<List<CyberNews>> snapshot) {
+    return Column(
+      children: <Widget>[
+        Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(5.0),
+          child: new Image.network(snapshot.data[index].urlToImage),
+        ),
+        Container(
+          height: MediaQuery.of(context).size.height * 0.5,
+          padding: EdgeInsets.all(40.0),
+          width: MediaQuery.of(context).size.width,
+          child: Center(
+            child: Text("test"),
+          ),
+        ),
+      ],
     );
   }
 }
